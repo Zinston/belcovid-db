b779f4cb2f998d75bcc43d9bf8e58c546c9e1449
Antoine Guenet <age@odoo.com>
2021-03-31 23:47:24 +0200

    setup express

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 import app from './app.js';
-import http from 'http';
+import express from 'express';
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
-// Create HTTP server and listen on port 3000 for requests
-const server = http.createServer((req, res) => {
+const server = express();
+server.get('/', function (req, res) {
+    // Set the response HTTP header with HTTP status and Content type
+    res.statusCode = 200;
+    res.send('Hello World\n');
+});
 
-  // Set the response HTTP header with HTTP status and Content type
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+// Handle 404 - Keep this as a last route
+server.use(function(req, res, next) {
+    res.status(404);
+    res.send('404: File Not Found');
 });
 
 // listen for request on port 3000, and as a callback function have the port listened on logged
