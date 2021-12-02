41465ccedb8cd4b5ae19cd8c11bfdf4dcd4bc194
Antoine Guenet <age@odoo.com>
2021-12-02 18:53:47 +0100

    use cors

--- src/index.js
+++ src/index.js
@@ -3,36 +3,35 @@ import MongoClient from 'mongodb';
 import {MONGO_URI} from './constants.js';
 import {getDiff} from './get.js';
 import {updateDatabase} from './put.js';
+import cors from 'cors';
 
 connectMongoDB().catch(console.error).then(client => {
 	const db = client.db('belcovid');
 
 	const server = express();
-	server.use((req, res, next) => {
-		res.header('Access-Control-Allow-Origin', '*');
-		next();
-	});
+	cors({credentials: true, origin: true});
+	server.use(cors());
 	server.get('/:key/:fromId', async (req, res) => {
 		// Get the diff between the data at given id and the latest data.
 		const diff = await getDiff(db, req.params.key, req.params.fromId);
 		res.statusCode = 200;
-		res.send(diff);
+		res.json(diff);
 	});
 	server.get('/update', async (req, res) => {
 		const updates = await updateDatabase(db);
 		res.statusCode = 200;
-		res.send({updated: updates});
+		res.json({updated: updates});
 	});
 	server.get('/update-time', async (req, res) => {
 		const updateTimes = await db.collection('lastUpdate').find().toArray();
 		res.statusCode = 200;
-		res.send(updateTimes[updateTimes.length - 1]);
+		res.json(updateTimes[updateTimes.length - 1]);
 	});
 	server.get('/:key', async (req, res) => {
 		// Get the full diff.
 		const diff = await getDiff(db, req.params.key);
 		res.statusCode = 200;
-		res.send(diff);
+		res.json(diff);
 	});
 
 	// Handle 404 - Keep this as a last route
